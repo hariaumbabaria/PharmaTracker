a105230f7eefa9c12af4f6dd76bc8616d03392ff
hansil18 <hansiljasoliya@gmail.com>
2021-06-26 16:49:12 +0530

    Update

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,20 @@
-import './App.css';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+//components
+import {} from 'react-bootstrap'
+import Header from './components/header/header';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      hello
-    </div>
-  );
+    <BrowserRouter>
+      <Header/>
+      <Switch>
+       {/* <Route exact path='/' component={Home} /> */}
+      </Switch>
+    </BrowserRouter>
+  )
 }
 
 export default App;
+
